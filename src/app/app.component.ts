1ef7ab807e3e5ae25bbd8adfc8a57a5b485e98a1
raulocampo1202 <69687202+raulocampo1202@users.noreply.github.com>
2021-05-24 15:12:18 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,19 +18,10 @@ import { throwIfEmpty } from 'rxjs/operators';
 })
 
 export class AppComponent implements OnInit {
-  public selectedIndex = 0;
-  public appPages = [];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-
-  // components
-  showMenu: boolean = true;
-  // searchShow: boolean = false;
-  loadingPlure: boolean = true;
+  
+  loadingPlure: boolean = false;
 
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
     private interceptService: InterceptService,
     private router: Router,
     private zone: NgZone
@@ -40,12 +31,10 @@ export class AppComponent implements OnInit {
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-
     this.onUniversalLink();
+  }
+  
+  ngOnInit() {    
   }
 
   onUniversalLink(): void{
@@ -54,63 +43,16 @@ export class AppComponent implements OnInit {
         const slug = data.url.split(".app").pop();
         if(slug)
           console.log(slug);
-      });
-    });
-  }
-
-  ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
-    this.onUserLogin();
-  }
+      });    
+    });  
+  }  
 
   // Initializes observable methods.
   initializeSubscribe() {
-    // method menu with change.
-    this.interceptService.newMenu$.subscribe(
-      obj => {
-        this.appPages = obj.menu;
-        this.showMenu = obj.showMenu;
-      }
-    );
-    // // search window with array.
-    // this.interceptService.searchShow$.subscribe(
-    //   obj => {
-    //     this.searchShow = obj.show;
-    //   }
-    // );
-    // loading wuindow with gif.
     this.interceptService.loading$.subscribe(
       bool => {
         this.loadingPlure = bool;
       }
     )
-  }
-
-  // Get Menu
-  onLoadMenu() {
-    this.interceptService.announceRouteMenu("Hola");
-  }
-
-  // identifies if the user is logged in plure with any user.
-  onUserLogin() {
-    // let userSession = JSON.parse(localStorage.getItem('userSession'));
-    // if ( userSession !== null && userSession !== undefined && location.pathname === '/login' ) { 
-    //   // verificar el fecha de expiracion.
-    //   this.router.navigateByUrl('init/home');
-    //   this.onTimeOut();
-    // } else if (location.pathname !== '' && location.pathname !== '/login' ) {
-    //   this.router.navigateByUrl('');
-    // }
-    this.loadingPlure = false;
-  }
-
-  onTimeOut() {
-    setTimeout(() => 
-    {
-      this.onUserLogin();
-    }, 20000);
   }
 }
