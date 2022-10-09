5a6b573fb00a8789101d74ccfc7efae97dcb3b06
Sebastian Murillas Gomez <s.murillas@hkgdutyfree.com>
2022-10-09 16:38:07 -0500

    divemdres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { AuthGuardService } from '@guard/auth-guard.service';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./public/public.module').then( m => m.PublicModule), canActivate: [AuthGuardService] },
-  { path: 'page', loadChildren: () => import('./private/private.module').then( m => m.PrivateModule), canActivate: [AuthGuardService] }
+  { path: 'page', loadChildren: () => import('./private/private.module').then( m => m.PrivateModule), canActivate: [AuthGuardService] },  {
+    path: 'physical-inventory',
+    loadChildren: () => import('./wms/physical-inventory/physical-inventory.module').then( m => m.PhysicalInventoryPageModule)
+  }
+
 ];
 
 @NgModule({
