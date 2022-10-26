e87637273c2a2e0eec1ca9d9eecb8c81da897a9f
Ivan Dario <dario.sanchez@mscloudexperts.com>
2022-10-26 17:54:09 -0500

    cambios final jueves

--- src/app/private/pages/wms/whitem-reclassification/whitem-reclassification.page.ts
+++ src/app/private/pages/wms/whitem-reclassification/whitem-reclassification.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 
@@ -9,12 +10,105 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 })
 export class WhitemReclassificationPage implements OnInit {
 
-  constructor(
-    private barcodeScanner: BarcodeScanner
+  public frm: FormGroup;
 
-  ) { }
+  public binCode:any = '';
+
+
+
+  constructor(
+    private barcodeScanner: BarcodeScanner,private formBuilder: FormBuilder
+
+  ) { 
+
+    this.frm = this.formBuilder.group(
+      {
+        bin: [' ', Validators.required],
+        qty: [0, Validators.required],
+        lpNo: [' ', Validators.required],
+  
+      }
+    )
+  }
 
   ngOnInit() {
+  }
+
+
+  onScanBin(){
+
+
+    this.barcodeScanner.scan().then(
+      barCodeData => {
+        let bin = barCodeData.text.toUpperCase();
+
+        this.binCode = barCodeData.text.toUpperCase();
+
+
+        this.frm.patchValue({
+
+          bin
+        })
+    
+      }
+    ).catch(
+      err => {
+        console.log(err);
+      }
+    )
+
+
+
+  }
+
+
+  add(){
+
+    let qty =  this.frm.get('qty').value;
+  
+      qty +=1
+  
+      this.frm.get('qty').setValue(qty);
+  
+    }
+  
+    res(){
+  
+      let qty =  this.frm.get('qty').value;
+  
+      if(qty > 0){
+   
+       qty -=1
+  
+       this.frm.get('qty').setValue(qty);
+       }
+  
+    }
+
+  onScanLP(){
+
+
+
+    this.barcodeScanner.scan().then(
+      barCodeData => {
+        let lpNo = barCodeData.text;
+
+
+        this.frm.patchValue({
+
+          lpNo
+        })
+      
+
+
+      }
+    ).catch(
+      err => {
+        console.log(err);
+      }
+    )
+
+
   }
   
 }
