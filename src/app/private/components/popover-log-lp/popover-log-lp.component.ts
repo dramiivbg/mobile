4ff7581c62a6726c31648f5c5e5e9c1f14fefc59
Sebastian Murillas Gomez <s.murillas@hkgdutyfree.com>
2022-10-28 16:59:45 -0500

    cambios

--- src/app/private/components/popover-log-lp/popover-log-lp.component.ts
+++ src/app/private/components/popover-log-lp/popover-log-lp.component.ts
@@ -2,13 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { InterceptService } from '@svc/intercept.service';
 
+
 @Component({
   selector: 'app-popover-log-lp',
   templateUrl: './popover-log-lp.component.html',
   styleUrls: ['./popover-log-lp.component.scss'],
 })
 export class PopoverLogLpComponent implements OnInit {
-
 
   @Input() logs: any = {} ;
  
