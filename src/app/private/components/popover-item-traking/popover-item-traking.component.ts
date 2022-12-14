0f0336a30c1253682fc6932ac5001a307cf15237
Sebastian Murillas Gomez <s.murillas@hkgdutyfree.com>
2022-12-14 16:15:04 -0500

    mas cambios poderosos

--- src/app/private/components/popover-item-traking/popover-item-traking.component.ts
+++ src/app/private/components/popover-item-traking/popover-item-traking.component.ts
@@ -8,33 +8,33 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PopoverItemTrakingComponent implements OnInit {
 
- 
+
   public frm: FormGroup;
   constructor(private formBuilder: FormBuilder) {
     this.frm = this.formBuilder.group(
       {
         Qty: ['0', Validators.required],
-        requestedDeliveryDate: ['',Validators.required],
-        TotalToReceive:[0, Validators.required],
-        SerialNo:['', Validators.required],
-        LotNo:['', Validators.required],
-        QtyBase:[0, Validators.required],
-        QtyHandleBase:[0, Validators.required],
-        
+        requestedDeliveryDate: ['', Validators.required],
+        TotalToReceive: [0, Validators.required],
+        SerialNo: ['', Validators.required],
+        LotNo: ['', Validators.required],
+        QtyBase: [0, Validators.required],
+        QtyHandleBase: [0, Validators.required],
 
 
-       
+
+
       }
     )
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 
-  calendar(){
+  calendar() {
 
-    
+
   }
-  
+
 
 }
