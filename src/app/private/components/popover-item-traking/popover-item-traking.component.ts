1b7e6ac44536e52f3cb7342d74a83dd662c4232a
Sebastian Murillas Gomez <s.murillas@hkgdutyfree.com>
2022-12-14 16:43:30 -0500

    Cambios buenos

--- src/app/private/components/popover-item-traking/popover-item-traking.component.ts
+++ src/app/private/components/popover-item-traking/popover-item-traking.component.ts
@@ -15,12 +15,12 @@ export class PopoverItemTrakingComponent implements OnInit {
     this.frm = this.formBuilder.group(
       {
         Qty: ['0', Validators.required],
-        requestedDeliveryDate: ['',Validators.required],
-        TotalToReceive:[0, Validators.required],
-        SerialNo:['', Validators.required],
-        LotNo:['', Validators.required],
-        QtyBase:[0, Validators.required],
-        QtyHandleBase:[0, Validators.required],
+        requestedDeliveryDate: ['', Validators.required],
+        TotalToReceive: [0, Validators.required],
+        SerialNo: ['', Validators.required],
+        LotNo: ['', Validators.required],
+        QtyBase: [0, Validators.required],
+        QtyHandleBase: [0, Validators.required],
 
 
 
@@ -31,7 +31,7 @@ export class PopoverItemTrakingComponent implements OnInit {
   ngOnInit() { }
 
 
- async closePopover(){
+  async closePopover() {
 
     this.popoverController.dismiss({});
   }
