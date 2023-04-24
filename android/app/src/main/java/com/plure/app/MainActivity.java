1e2c2f5924e39353e558a4c43e1d539d83558814
Juan Pena <juan.pena@mscloudexperts.com>
2023-04-24 09:52:49 -0500

    Se actualiza el gradle

--- android/app/src/main/java/com/plure/app/MainActivity.java
+++ android/app/src/main/java/com/plure/app/MainActivity.java
@@ -1,15 +1,5 @@
 package com.plure.app;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
 
-import java.util.ArrayList;
-
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
-}
+public class MainActivity extends BridgeActivity {}
